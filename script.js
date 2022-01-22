72e4de03306146164b27b5597e86f66dda60b1ef
Tenq Boersma <33595@ma-web.nl>
2022-01-22 19:13:12 +0100

    gefixte route

--- script.js
+++ script.js
@@ -1,88 +1,71 @@
-const MyTitle = document.getElementById('MyTitle');
-const myimage = document.getElementById('myImage');
-const myInput = document.getElementById('myInput');
+const myTitle = document.getElementById("myTitle");
+const myImage = document.getElementById("myImage");
+const myInput = document.getElementById("myInput");
 
-let lokaties = 
-[   
+let lokaties = [
     {
-        "titel": "Havenstad",
-        "Description": "De havenstad"
-        "image": "foto/Havenstad.jpg",
-        "descriptions": {
-            "verder": 1
-        }
+        "titel":"Begin",
+        "image":"foto/windroos.png"
     },
     {
-        "titel": "Verhaal van de Walvis",
-        "Description": "de geschiedenis van de walvis en walvis jacht"
-        "image": "foto/Walvisverhaal.jpg",
-        "descriptions": {
-            "verder": 2
-                    "terug": 0
-        }
-
+        "titel":"Havenstad",
+        "image":"foto/Havenstad.jpg"
     },
     {
-        "titel": "Willem van de Velde & Zoon",
-        "Description": "een gallerij vol kunsten van Willem van de Velde en zijn Zoon"
-        "image": "foto/WillemEnZoon.jpg",
+        "titel":"Verhaal van de Walvis",
+        "image":"foto/Walvisverhaal.jpg"
     },
     {
-        "titel": "Hoofd gallerij",
-        "Description": "Hier ziet u ..."
-        "image": "foto/Hoofdgallerij.jpg",
+        "titel":"Willem van de Velde & Zoon",
+        "image":"foto/WillemEnZoon.jpg"
     },
     {
-        "titel": "VR Experience",
-        "Description": "Hier ziet u ..."
-        "image": "foto/VR room.jpg",
+        "titel":"Hoofd Gallerij",
+        "image":"foto/Hoofdgallerij.jpg"
     },
     {
-        "titel": "Koningssloep",
-        "Description": "Hier ziet u ..."
-        "image": "foto/Koningssloep.jpg",
+        "titel":"VR Experience",
+        "image":"foto/VR room.jpg"
     },
     {
-        "titel": "VOC Schip Amsterdam",
-        "Description": "Hier ziet u ..."
-        "image": "foto/VOCschip.jpg",
+        "titel":"Koningssloep",
+        "image":"foto/Koningssloep.jpg"
     },
     {
-        "titel": "Stoomschip Christiaan Brunings",
-        "Description": "Hier ziet u ..."
-        "image": "foto/ChristiaanBrunings.jpg",
+        "titel":"VOC Schip Amsterdam",
+        "image":"foto/VOCschip.jpg"
     },
     {
-        "titel": "Jachtmodellen",
-        "Description": "Hier ziet u ..."
-        "image": "foto/Jachtmodellen.jpg",
+        "titel":"Stoomschip Christiaan Brunings",
+        "image":"foto/ChristiaanBrunings.jpg"
     },
     {
-        "titel": "Navigatie Instrumenten",
-        "Description": "Hier ziet u ..."
-        "image": "foto/NavigatieInstrumenten.jpg",
+        "titel":"Jachtmodellen",
+        "image":"foto/Jachtmodellen.jpg"
     },
     {
-        "titel": "Scheepsornamenten",
-        "Description": "Hier ziet u ..."
-        "image": "foto/Ornamenten.jpg",
+        "titel":"Navigatie Instrumenten",
+        "image":"foto/NavigatieInstrumenten.jpg"
     },
     {
-        "titel": "Verloren Gewaand Manuscript",
-        "Description": "Hier ziet u ..."
-        "image": "Manuscript.jpg",
+        "titel":"Scheeps Ornamenten",
+        "image":"foto/Ornamenten.jpg"
+    },
+    {
+        "titel":"Een verloren gewaand manuscript",
+        "image":"foto/Manuscript.jpg"
     }
-];
 
-function show(index)){
-    MyTitle.innerHTML = lokaties[index].titel;
-    myimage.src = lokaties[index].image;
+]
+
+function show(index){
+    myTitle.innerHTML = lokaties[index].titel;
+    myImage.src = lokaties[index].image;
 }
 
 function getInput(){
-    show(myInput.value);
-    myInput.value = "";
-    myInput.focus();
+    show(myInput.value)
+    console.log(myInput.value)
 }
 
 show(0);
