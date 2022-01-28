20caf8db75d8ef0b3a96ec5db010a6edb39bb13b
Tenq Boersma <33595@ma-web.nl>
2022-01-28 11:43:03 +0100

    Script fix

--- englishscript.js
+++ englishscript.js
@@ -7,81 +7,81 @@ const myInput = document.getElementById("myInput");
 let lokaties = [
     {
         "titel":"start",
-        "image":"foto/windroos.png",
+        "image":"Foto/windroos.png",
         "text":"where do you want to go to?",
         "text2":"1 = Westwing, 3 = Northwing, 9 = Eastwing"
     },
     {
         "titel":"Havenstad",
-        "image":"foto/Havenstad.jpg",
+        "image":"Foto/Havenstad.jpg",
         "text":"equipment from the 1700s.",
         "text2":"2 = story of the whale, 0 = back to start"
     },
     {
         "titel":"Story of the whale",
-        "image":"foto/Walvisverhaal.jpg",
+        "image":"Foto/Walvisverhaal.jpg",
         "text":"The history of whale hunting",
         "text2":"0 = back to start, 1 = move back to Havenstad"
 
     },
     {
         "titel":"Willem van de Velde & Son",
-        "image":"foto/WillemEnZoon.jpg",
+        "image":"Foto/WillemEnZoon.jpg",
         "text":"Paintings from Willem vd Velde and his son",
         "text2":"4 = Move further to the Main gallery, 0 = go back to start"
 
     },
     {
         "titel":"Main Gallery",
-        "image":"foto/Hoofdgallerij.jpg",
+        "image":"Foto/Hoofdgallerij.jpg",
         "text":"Gallery of art from the 1700s",
         "text2":"5 = move further to the VR experience, 4 = move back to willem vd Velde & Son"
     },
     {
         "titel":"VR Experience",
-        "image":"foto/VR room.jpg",
+        "image":"Foto/VR room.jpg",
         "text":"Journey through the time of the VOC",
         "text2":"6 = Move further to koningssloep, 5 = move back to the Main Gallery"
     },
     {
         "titel":"Koningssloep",
-        "image":"foto/Koningssloep.jpg",
+        "image":"Foto/Koningssloep.jpg",
         "text":"See a ship that has been used by different royalties",
         "text2":"7 = move further to VOC Schip, 5 = move back to VR experience"
     },
     {
         "titel":"VOC Ship Amsterdam",
-        "image":"foto/VOCschip.jpg",
+        "image":"Foto/VOCschip.jpg",
         "text":"Replica of a ship that sunk in 1749",
         "text2":"8 = Move further toStoomschip Christiaan Brunings, 6 = Move back to Koningssloep"
     },
     {
         "titel":"Steamboat Christiaan Brunings",
-        "image":"foto/ChristiaanBrunings.jpg",
+        "image":"Foto/ChristiaanBrunings.jpg",
         "text":"Steamboat that was used in 1900",
         "text2":"0 = Move back to start, 7 = move back to VOC schip"
     },
     {
         "titel":"Hunting Models",
-        "image":"foto/Jachtmodellen.jpg",
+        "image":"Foto/Jachtmodellen.jpg",
         "text":"made to scale copies of different ships from the time of the VOC",
         "text2":"10 = move further to Navigatie Instrumenten, 0 = move back to start"
     },
     {
         "titel":"Navigation Instruments",
-        "image":"foto/NavigatieInstrumenten.jpg",
+        "image":"Foto/NavigatieInstrumenten.jpg",
         "text":"Equipment that was used vor navigation on boats",
         "text2":"11 = move further to Scheepsornamenten, 9 = move back to huntingmodels"
     },
     {
         "titel":"Ships Ornaments",
-        "image":"foto/Ornamenten.jpg",
+        "image":"Foto/Ornamenten.jpg",
         "text":"schip decoration",
         "text2":"12 = move further to a lost thought manuscript, 10 = move back to navigation instruments"
     },
     {
         "titel":"A lost thought manuscript",
-        "image":"foto/Manuscript.jpg",
+        "image":"Foto/Manuscript.jpg",
         "text":"Old texts from the Banda-Islands",
         "text2":"0 = move back to start, 11 = move back to Schips Ornaments"
     }
--- script.js
+++ script.js
@@ -7,81 +7,81 @@ const myInput = document.getElementById("myInput");
 let lokaties = [
     {
         "titel":"Begin",
-        "image":"foto/windroos.png",
+        "image":"Foto/windroos.png",
         "text":"Welke kant wilt u op?",
         "text2":"1 = Westvleugel, 3 = Noordvleugel, 9 = Oostvleugel"
     },
     {
         "titel":"Havenstad",
-        "image":"foto/Havenstad.jpg",
+        "image":"Foto/Havenstad.jpg",
         "text":"Hier staan apperaturen die in 1700 werden gebruikt.",
         "text2":"2 gaat naar Verhaal van de walvis, 0 gaat terug naar begin"
     },
     {
         "titel":"Verhaal van de Walvis",
-        "image":"foto/Walvisverhaal.jpg",
+        "image":"Foto/Walvisverhaal.jpg",
         "text":"De geschiedenis va walvisjacht.",
         "text2":"0 gaat terug naar begin, 1 gaat terug naar havenstad."
 
     },
     {
         "titel":"Willem van de Velde & Zoon",
-        "image":"foto/WillemEnZoon.jpg",
+        "image":"Foto/WillemEnZoon.jpg",
         "text":"Schilderijen van Willem van de Velde en zijn zoon.",
         "text2":"4 gaat verder naar de Hoofdgallerij, 0 gaat terug naar begin."
 
     },
     {
         "titel":"Hoofd Gallerij",
-        "image":"foto/Hoofdgallerij.jpg",
+        "image":"Foto/Hoofdgallerij.jpg",
         "text":"Gallerij van kunst uit de tijd van de VOC.",
         "text2":"5 = verder naar VR experience, 4 = terug naar Willem V.D & Zoon"
     },
     {
         "titel":"VR Experience",
-        "image":"foto/VR room.jpg",
+        "image":"Foto/VR room.jpg",
         "text":"Reis met een VR headset door de tijd van de VOC.",
         "text2":"6 = verder naar Koningssloep, 5 = Terug naar Hoofdgallerij"
     },
     {
         "titel":"Koningssloep",
-        "image":"foto/Koningssloep.jpg",
+        "image":"Foto/Koningssloep.jpg",
         "text":"Bekijk het schip die door meerdere koningklijkheden is gebruikt.",
         "text2":"7 = verder naar VOC Schip, 5 = terug naar VR experience"
     },
     {
         "titel":"VOC Schip Amsterdam",
-        "image":"foto/VOCschip.jpg",
+        "image":"Foto/VOCschip.jpg",
         "text":"Een  schip dat verloren is gegaan in 1749, waar nu een replica van is gemaakt",
         "text2":"8 = verder naar Stoomschip Christiaan Brunings, 6 = terug naar Koningssloep"
     },
     {
         "titel":"Stoomschip Christiaan Brunings",
-        "image":"foto/ChristiaanBrunings.jpg",
+        "image":"Foto/ChristiaanBrunings.jpg",
         "text":"Stoomboot die is gebouwd in 1900",
         "text2":"9 = terug naar begin, 7 = terug naar VOC schip"
     },
     {
         "titel":"Jachtmodellen",
-        "image":"foto/Jachtmodellen.jpg",
+        "image":"Foto/Jachtmodellen.jpg",
         "text":"op schaal gemaakte kopies van gebruikten jachtschepen",
         "text2":"10 = verder naar Navigatie Instrumenten, 0 = terug naar begin"
     },
     {
         "titel":"Navigatie Instrumenten",
-        "image":"foto/NavigatieInstrumenten.jpg",
+        "image":"Foto/NavigatieInstrumenten.jpg",
         "text":"Apperatuur dat werd gebruikt voor Navigatie op boten",
         "text2":"11 = verder naar Scheepsornamenten, 9 = terug naar jachtmodellen"
     },
     {
         "titel":"Scheeps Ornamenten",
-        "image":"foto/Ornamenten.jpg",
+        "image":"Foto/Ornamenten.jpg",
         "text":"scheepsversiering",
         "text2":"12 = verder naar een verloren gewaand manuscript, 10 = terug naar navigatie instrumenten"
     },
     {
         "titel":"Een verloren gewaand manuscript",
-        "image":"foto/Manuscript.jpg",
+        "image":"Foto/Manuscript.jpg",
         "text":"oude teksten over de Banda-eilanden",
         "text2":"0 = verder naar begin, 11 = terug naar Scheeps Ornamenten"
     }
